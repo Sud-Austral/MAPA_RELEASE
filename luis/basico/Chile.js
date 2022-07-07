d1e3802b62cb948fd328a275a9bc65ac6c2f008a
lmonsalve22 <lmonsalve22@gmail.com>
2022-07-07 16:22:56 -0400

    basico cerrado

--- luis/basico/Chile.js
+++ luis/basico/Chile.js
@@ -1,16 +1,15 @@
-// coordenadas y luego de la coma el zoom inicial que queremos.
-let map = L.map("map1").setView([-33.458725187656356, -70.66008634501547],10);
+//console.log(producto);
 
-//El mapa a utilizar y los valores correspondientes a este.
-var base = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(map);
+const cuerposagua = getData("https://raw.githubusercontent.com/Sud-Austral/DATA_MAPA_PUBLIC_V2/main/AGUAS/CUERPOS/cuerpos_agua_09201.geojson");
+
+//chile.features= chile.features.filter (x=>x.properties.CUT_COM == "09201")
+let comunas_filtro = ["09101","09102","09103","09104","09201","09202","09203","09204","10301","10302","10303","10304"];
+//chile.features = chile.features.filter(x => comunas_filtro.indexOf(x.properties.CUT_COM)> -1);
+
+
+
+
+
 
 //PopUp automatico
 const customOptions =
@@ -125,49 +124,14 @@ var overlayMapsRegiones = {
    "Comunas 09": chileJS
 };
 
-        // MAPA BASE 2
-        var base2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/dark-v9',
-            tileSize: 512,
-            zoomOffset: -1
-        });
-
-        // MAPA BASE 3
-        var base3 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/satellite-streets-v9',
-            tileSize: 512,
-            zoomOffset: -1
-        });
-        //Funcion para los botones inferiores, cambiar mapa
-        $(document).ready(function() {
-            $('.op1').on('click', function(){
-                base2.remove();
-                base3.remove();
-                base.addTo(map);
-            });
-            $('.op2').on('click', function(){
-                base.remove();
-                base3.remove();
-                base2.addTo(map);
-            });
-            $('.op3').on('click', function(){
-                base.remove();
-                base2.remove();
-                base3.remove();
-                base3.addTo(map);
-            });
-        });
         
-        var capas_base = {"Mapa claro":base, "Mapa Oscuro":base2, "Mapa Satelital":base3}
+        
+var capas_base = {"Mapa claro":base, "Mapa Oscuro":base2, "Mapa Satelital":base3}
 
-        var capaRegiones2 = L.control.layers(capas_base, overlayMapsRegiones, {
-            position: 'topright', // 'topleft', 'bottomleft', 'bottomright'
-            collapsed: false // true
-        }).addTo(map);
+var capaRegiones2 = L.control.layers(capas_base, overlayMapsRegiones, {
+    position: 'topright', // 'topleft', 'bottomleft', 'bottomright'
+    collapsed: false // true
+}).addTo(map);
 
 
  // Para centrar en el objeto que uno quiera. Las 4
