38bb1229500164e5ecb62683d088ff78239ec9cd
lmonsalve22 <lmonsalve22@gmail.com>
2022-09-13 15:52:26 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -9,22 +9,27 @@ import os
 def main():
     df = pd.read_excel("https://github.com/Sud-Austral/MAPA_RELEASE/blob/main/origenes/Origenes%20Mapas.xlsx?raw=true")
     for i,j in df.iterrows():
-        print(j["nombre"])
+        
         #copy_tree(fr"{os.getcwd()}/luis/bases/main", fr"{os.getcwd()}/publicaciones2/{j['nombre']}")
         copy_tree("luis/bases/main",f"publicaciones2/{j['nombre']}")
         ref1 = pd.read_excel(j["excel"], sheet_name="BASE Global")
         ref2 = pd.read_excel(j["excel"], sheet_name="Capas")
+
+        with open(f"publicaciones2/{j['nombre']}/db", 'w', encoding='utf-8') as file:
+            ref1.to_json(file,orient="records",force_ascii=False)
+
+        with open(f"publicaciones2/{j['nombre']}/db", 'w', encoding='utf-8') as file:
+            ref2.to_json(file,orient="records",force_ascii=False)
+
+        print(j["nombre"])
         """
         
 
-        with open(fr"publicaciones2/{j['nombre']}/db", 'w', encoding='utf-8') as file:
-            ref1.to_json(file,orient="records",force_ascii=False)
-
+        
         
 
 
-        with open(fr"publicaciones2/{j['nombre']}/db", 'w', encoding='utf-8') as file:
-            ref2.to_json(file,orient="records",force_ascii=False)
+        
         """
         return
 
