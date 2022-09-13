698e8aa13c507ab0e5e2b9fb60a2ca458dd02e8e
lmonsalve22 <lmonsalve22@gmail.com>
2022-09-13 15:34:18 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -4,12 +4,13 @@ import pandas as pd
 import datetime
 import sys
 from distutils.dir_util import copy_tree
+import os
 
 def main():
     df = pd.read_excel("https://github.com/Sud-Austral/MAPA_RELEASE/blob/main/origenes/Origenes%20Mapas.xlsx?raw=true")
     for i,j in df.iterrows():
         print(j["nombre"])
-        copy_tree(r"luis\bases\main", fr"publicaciones2/{j['nombre']}")
+        copy_tree(fr"{os.getcwd()}/luis\bases\main", fr"{os.getcwd()}/publicaciones2/{j['nombre']}")
         ref1 = pd.read_excel(j["excel"], sheet_name="BASE Global")
 
         with open(fr"publicaciones2/{j['nombre']}/db", 'w', encoding='utf-8') as file:
