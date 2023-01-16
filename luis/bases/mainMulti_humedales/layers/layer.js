fd608785964cee24dfcb626368911717b187b3d4
lmonsalve22 <lmonsalve22@gmail.com>
2023-01-16 18:43:15 -0300

    humedales

---
+++ luis/bases/mainMulti_humedales/layers/layer.js
@@ -0,0 +1,185 @@
+class layerSingle {
+    constructor(capa) {
+      this.capa = capa;
+      this.objReferencia = dataGlobal.filter(x => x["descripcion_capa"] == capa["nombre"])[0];
+      this.objReferenciaGlobal = dataCapaGlobal.filter(x => x.idcapa == this.objReferencia.idcapa)[0];
+      //console.log(this.objReferenciaGlobal)
+      this.data = {};
+      this.jsonColoresRandom = null;
+      this.legend = null;
+      this.geojson = null;
+    }
+
+    
+
+    getLegend(Comunas){
+        let dataAcumulada = []
+        let dataAcumuladoFull = []
+        Comunas.forEach(x =>{
+            try {
+                let auxArray = this.data[x.codComuna].data.features.map(x => x["properties"]);                
+                dataAcumuladoFull = dataAcumuladoFull.concat(this.data[x.codComuna].data.features);
+                dataAcumulada = dataAcumulada.concat(auxArray);    
+            } catch (error) {
+                console.log("Revisar ",x,this.objReferencia)
+            }      
+        });
+        
+        let objReferencia = this.objReferencia;
+        let variableUnica =  objReferencia["Propiedad"]; 
+        let unicos = dataAcumulada.map(x => x[variableUnica]);
+        unicos = [...new Set(unicos)];
+        console.log("unicos",unicos)
+        if(objReferencia["Tipo"] == "Puntos"){
+            //Esto es un punto
+            if(objReferencia["Variable"] == "random"){
+                //Esto es un punto random
+                let paleta = getPalletteIcon(objReferencia);
+                let result = getHtmlFromPointRandom2(unicos,paleta,objReferencia,variableUnica);
+                let htmlString = result[0];
+                this.jsonColoresRandom = result[1]
+                this.legend = getLegendLeaflet2(htmlString);
+            }
+            else{                
+                //Esto es un punto Definido
+                let result = getHtmlFromPointDefinidos2(variableUnica,objReferencia)
+                let htmlString = result[0]
+                this.jsonColoresRandom = result[1]
+                this.legend = getLegendLeaflet2(htmlString);                        
+            }
+            this.getMapaPoint(dataAcumuladoFull)
+        }
+        else{
+            //Esto es un poligono
+            if(objReferencia["Variable"] == "random"){
+                //Esto es un poligono random
+                
+                let paleta = getPallette(objReferencia);
+                let result = getHtmlFromPoligonRandom2(unicos,paleta,objReferencia,variableUnica);
+                let htmlString = result[0]
+                this.jsonColoresRandom = result[1]
+                this.legend = getLegendLeaflet2(htmlString);
+            }
+            else{
+                
+                //"Esto es un poligono con colores definidos"
+                let result = getHtmlFromPoligonDefinidos2(variableUnica,objReferencia,unicos)
+                let htmlString = result[0]
+                this.jsonColoresRandom = result[1]
+                this.legend = getLegendLeaflet2(htmlString);                        
+            }
+            this.getMapaPoligono(dataAcumuladoFull);
+        }
+        
+    }
+
+    getMapaPoint(dataAcumuladoFull){
+        let dataGlobalNivel2 = dataGlobal.filter( capaGlobal =>
+            capaGlobal.idcapa == this.objReferencia["idcapa"]
+        );
+        //console.log(dataGlobalNivel2)
+        let dataGlobalDescripCapaUnique = [...new Set(dataGlobalNivel2
+            .filter(x => x.popup_0_1 != null)
+            .sort((x,y) => {return x["posición_capa"] - y["posición_capa"]})
+            .map(x => x.descripcion_capa)
+            .filter(x => x)
+            )];
+    
+        let dataGlobalPropiedadesUnique = [... new Set(dataGlobalNivel2.sort( x=> x.posicion_popup).map(x => x.Propiedad))]
+        let diccionarioNombrePropiedadPopup = {}
+        dataGlobalNivel2.forEach(x => diccionarioNombrePropiedadPopup[x.Propiedad] = x["descripcion_pop-up"]);
+              
+        let onEachFeatureCustom = (feature, layer) =>{
+            let htmlString = dataGlobalPropiedadesUnique.map(element => getStringHTML4(feature, element,diccionarioNombrePropiedadPopup[element])).toString();
+            htmlString = htmlString.replaceAll(",", "")
+            htmlString = htmlString + 
+            "</div><center><img class='banner2' src='https://raw.githubusercontent.com/Sud-Austral/mapa_glaciares/main/img/logo_DataIntelligence_normal.png' alt='Data Intelligence'/></center>";
+            layer.bindPopup("<div class='parrafo_popup'>" + htmlString + "</div>", customOptions);                
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature,
+            })
+        }
+        let propiedad = this.objReferencia["Propiedad"]
+
+        let setIcon = (feature, latlng) =>{
+            let valorPropiedad =feature["properties"][propiedad];         
+            let color = this.jsonColoresRandom[valorPropiedad];
+            let myIcon = getIcon(color);
+            return L.marker(latlng, { icon: myIcon });
+        }
+
+        if(this.objReferencia["Variable"] == "default"){
+            setIcon = (feature, latlng) =>{
+                let url = this.objReferencia["url_icono"];
+                let myIcon = getIcon(url);
+                return L.marker(latlng, { icon: myIcon });
+            }
+        }
+        let geojson = L.geoJson({"type": 'FeatureCollection',
+        "features":dataAcumuladoFull},//{style:estiloDinamico}).addTo(map);
+        {onEachFeature: onEachFeatureCustom,pointToLayer: setIcon}).addTo(map);
+        this.geojson = geojson;
+    }
+
+    getMapaPoligono(dataAcumuladoFull){
+        let propiedad = this.objReferencia["Propiedad"]
+        
+        console.log(this.objReferencia)
+
+        let estiloDinamico = (feature) => {  
+            let valorPropiedad =feature["properties"][propiedad]?feature["properties"][propiedad]:"Sin Información"          
+            let color = this.jsonColoresRandom[valorPropiedad];
+            
+            return {"fillOpacity":0.7,"opacity":0.75,"color":color}
+        }
+        if(this.objReferencia["Variable"] == "default"){
+            estiloDinamico = () =>{
+                let color = this.objReferencia["Color"];
+                return {"fillOpacity":0.7,"opacity":0.75,"color":color}
+            }
+        }
+        //console.log(dataGlobal,this.objReferencia,this.objReferenciaGlobal)
+        let dataGlobalNivel2 = dataGlobal.filter( capaGlobal =>
+            capaGlobal.idcapa == this.objReferencia["idcapa"]
+        );
+        
+        let dataGlobalDescripCapaUnique = [...new Set(dataGlobalNivel2
+            .filter(x => x.popup_0_1 != null)
+            .sort((x,y) => {return x["posición_capa"] - y["posición_capa"]})
+            .map(x => x.descripcion_capa)
+            .filter(x => x)
+            )];
+    
+        let dataGlobalPropiedadesUnique = [... new Set(dataGlobalNivel2.sort( x=> x.posicion_popup).map(x => x.Propiedad))]
+        let diccionarioNombrePropiedadPopup = {}
+        dataGlobalNivel2.forEach(x => diccionarioNombrePropiedadPopup[x.Propiedad] = x["descripcion_pop-up"]);
+        
+             
+        let onEachFeatureCustom = (feature, layer) =>{
+            let htmlString = dataGlobalPropiedadesUnique.map(element => getStringHTML4(feature, element,diccionarioNombrePropiedadPopup[element])).toString();
+            htmlString = htmlString.replaceAll(",", "")
+            htmlString = htmlString + 
+            "</div><center><img class='banner2' src='https://raw.githubusercontent.com/Sud-Austral/mapa_glaciares/main/img/logo_DataIntelligence_normal.png' alt='Data Intelligence'/></center>";
+            layer.bindPopup("<div class='parrafo_popup'>" + htmlString + "</div>", customOptions);                
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature,
+            })
+        }
+
+        console.log("aki 2",dataAcumuladoFull,dataGlobalNivel2) 
+        let geojson = L.geoJson({"type": 'FeatureCollection',
+        "features":dataAcumuladoFull},
+        {style:estiloDinamico,onEachFeature: onEachFeatureCustom}).addTo(map);
+        /*
+        let geojson = L.geoJson({"type": 'FeatureCollection',
+        "features":dataAcumuladoFull},
+        {style:estiloDinamico,onEachFeature: onEachFeatureCustom}).addTo(map);
+        */
+        this.geojson = geojson;
+    }
+
+}
