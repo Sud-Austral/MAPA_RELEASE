31938f233ce708c48080a147408f8ff2f03de679
lmonsalve22 <lmonsalve22@gmail.com>
2023-02-08 19:30:55 -0300

    Update clases.js

--- publicaciones3/incendio_jv/script/clases.js
+++ publicaciones3/incendio_jv/script/clases.js
@@ -652,6 +652,7 @@ class MultiMap{
                         click: zoomToFeature,
                     })
                 }
+                console.log("Geometria",capa["data"])
                 let tipoGeometria = capa["data"]["features"][0]["geometry"]["type"];
                 let arraForEach = dataGlobalDescripCapaUnique
                                     .sort(x => x["posición_capa"])
